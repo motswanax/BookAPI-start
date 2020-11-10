6a6d6dc9e9e2630ada20039d0d78bae34e31d746
bdijo <bdijo@D-IMEC-TRAINEE->
2020-11-10 10:47:52 +0200

    Added CountryController to Controllers. Injected ICountryRepositoryGUI into it. Added the service in Startup. Changed all interfaces in Services to public.

---
+++ BookGUI/Controllers/CountriesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookGUI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookGUI.Controllers
+{
+    public class CountriesController : Controller
+    {
+        ICountryRepositoryGUI countryRepository;
+
+        // Constructor dependency injection. Injected in Startup.ConfigureServices();
+        public CountriesController(ICountryRepositoryGUI countryRepository)
+        {
+            this.countryRepository = countryRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var countries = countryRepository.GetCountries();
+
+            if (countries.Count() <= 0)
+            {
+                ViewBag.Message = "There was a problem retrieving countries from the " +
+                    "database or no country exists. ";
+            }
+
+            return View(countries);
+        }
+    }
+}
